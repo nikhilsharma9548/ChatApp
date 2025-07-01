e9ff250890405f1f7e77fcf23c7b3fa893882933
nikhilsharma9548 <ns630403@gmail.com>
2025-07-01 19:58:08 +0530

    done

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -17,13 +17,13 @@ const Main = () => {
   return (
 
     <>
-        <div className='w-full h-[100%] bg-gradient-to-r from-[#1E1E1E] to-[#121212]  text-white'>
+        <div className='w-full bg-gradient-to-r from-[#1E1E1E] to-[#121212]  text-white'>
             <div className="md:px-10 px-3 py-5 text-2xl flex items-center  justify-between">
                 <p>ChatBot</p>
                 <p><FaRegUser /></p>
             </div>
-
-            <div className=' w-full h-[80%]  overflow-y-auto hide-scrollbar'>
+            <div className='flex min-h-full flex-col  justify-between'>
+                 <div className=' w-full overflow-y-auto hide-scrollbar'>
                 {! showResult ? (
                     <div className='flex flex-col text-4xl sm:text-6xl max-sm:px-5 max-sm:py-24  sm:p-36'>
                         <p><span className='bg-gradient-to-r from-[#4b90ff] to-[#ff5546] bg-clip-text text-transparent'>hello, there</span></p>
@@ -50,7 +50,7 @@ const Main = () => {
 
             {/* bottom field  */}
 
-            <div className='w-full flex sticky flex-col justify-center pb-20 items-center h-[20%]'>
+            <div className='w-full flex  flex-col pb-28 items-center '>
                 <div className='md:text-2xl text-lg flex w-[97%] md:w-[60%] bg-gray-500/50  rounded-full'>
                     <input type="text"
                     value={input}
@@ -63,6 +63,8 @@ const Main = () => {
                         className='flex justify-center items-center pr-5 cursor-pointer'><IoSendSharp /></p>
                 </div>
             </div>
+            </div>
+           
         </div>
     </>
     // <div className='w-full'>
