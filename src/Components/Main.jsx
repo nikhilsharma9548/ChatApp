6a50ba7cd6093feee26b848f2233140f20e6a208
nikhilsharma9548 <ns630403@gmail.com>
2025-07-02 18:40:29 +0530

    done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -15,19 +15,19 @@ const Main = () => {
   return (
 
     <>
-        <div className='w-full bg-gradient-to-r flex flex-col justify-between from-[#1E1E1E] to-[#121212]  text-white'>
+        <div className='w-full flex flex-col justify-between bg-gradient-to-r  from-[#1E1E1E] to-[#121212]  text-white'>
             <div className="md:px-10 px-3 py-5 text-2xl flex items-center  justify-between">
                 <p>ChatBot</p>
                 <p><FaRegUser /></p>
             </div>
-             <div className=' w-full overflow-y-auto hide-scrollbar'>
+             <div className=' w-full flex-1 overflow-y-auto hide-scrollbar'>
                 {! showResult ? (
-                    <div className='flex flex-col text-4xl sm:text-6xl max-sm:px-5 max-sm:py-24  sm:p-36'>
+                    <div className='flex flex-col text-4xl sm:text-6xl  sm:p-24'>
                         <p><span className='bg-gradient-to-r from-[#4b90ff] to-[#ff5546] bg-clip-text text-transparent'>hello, there</span></p>
                         <p>How can I help you Today?</p>
                     </div>
                 ):
-                <div className='md:px-32 px-10 py-10 flex gap-5'>
+                <div className='md:px-32 px-5 items-center py-10 flex gap-3'>
                     <p className=' text-2xl'><FaMailchimp /></p>
                                      {loading ?(
                         <div className='h-3 w-3 flex gap-3'>
@@ -38,8 +38,6 @@ const Main = () => {
                     ):(<p dangerouslySetInnerHTML={{ __html: resultData || "No data" }} 
                     className=''
                     ></p>
-
-
                     )}
                 </div>
                 }
@@ -47,10 +45,13 @@ const Main = () => {
 
             {/* bottom field  */}
 
-            <div className='w-full flex  flex-col md:pb-28 pb-40 items-center'>
-                <div className='md:text-2xl text-lg flex w-[90%] md:w-[60%] fixed bg-gray-500/50  rounded-full'>
+            <div className='w-full flex  flex-col pb-10 items-center'>
+                <div className='md:text-2xl text-lg flex w-[90%] md:w-[60%] sticky  bg-gray-500/50  rounded-full'>
                     <input type="text"
                     value={input}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') onSent();
+                     }}
                     onChange={(e) =>setInput(e.target.value)}
                     placeholder='Ask anythink.....'
                     className='bg-transparent  border-none p-3 outline-none w-full' />
